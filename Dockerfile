7118a9a58184df91bcc49ec067e06dd792e79419
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2026-02-06 10:50:18 +0000

    update solution task 2

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,9 @@
 FROM public.ecr.aws/dataminded/spark-k8s-glue:v3.5.4-hadoop-3.3.6-v1
 
 USER 0
-ENV PYSPARK_PYTHON=python3
-ENV PYTHONPATH=/opt/spark/work-dir/src
+ENV PYSPARK_PYTHON python3
 WORKDIR /opt/spark/work-dir
 
-RUN ln -sf /usr/bin/python3 /usr/bin/python
-
-COPY requirements.txt .
-RUN pip3 install --no-cache-dir -r requirements.txt
-
 COPY src/ ./src/
+COPY pyproject.toml .
+RUN pip3 install .
--- dags/clean_dag.py
+++ dags/clean_dag.py
@@ -5,7 +5,9 @@ import os
 
 default_args = {
     'owner': 'airflow',
+    "description": "Use of the DockerOperator",
     'depends_on_past': False,
+    "start_date": datetime(2021, 5, 1),
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -15,19 +17,17 @@ default_args = {
 with DAG(
     'capstone_llm_clean',
     default_args=default_args,
-    description='Clean questions and answers',
-    schedule=None,
-    start_date=datetime(2026, 2, 5),
+    schedule="5 * * * *",
     catchup=False,
 ) as dag:
 
     clean_task = DockerOperator(
-        task_id='clean',
+        task_id='clean_task',
         image='capstone-llm-clean:latest',
         container_name='capstone_llm_clean_task',
         api_version='auto',
         auto_remove='force',
-        command='python -m capstonellm.tasks.clean -t pyspark -e production',
+        command='python -m capstonellm.tasks.clean -t docker',
         docker_url='unix://var/run/docker.sock',
         network_mode='bridge',
         environment={
